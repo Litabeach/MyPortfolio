dbcc0b29711970dca9d43e46cb20993a46d7c34c
Lita Beach <litabeach123@gmail.com>
2021-05-03 11:26:17 -0500

    pages basic structure set up

---
+++ src/pages/Contact.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+function Contact() {
+    
+  
+      return (
+
+        //       <!-- contact info -->
+        //       <h3>Contact Lita:</h3> 
+        //       <h5>Email:</h5>
+        //       <a href="mailto:litabeach123@gmail.com" class="links">
+        //         <img class="img img thumbnail icon" src="02-CSS-Bootstrap/Assets/Images2/email.jpg" alt="email icon">
+        //      litabeach123@gmail.com   </a> <br> 
+        //       <h5>Phone:</h5>
+        //       <a href="tel:612-558-4804" class="links">
+        //         <img class="img img thumbnail icon" src="02-CSS-Bootstrap/Assets/Images2/phone.jpg" alt ="phone icon">
+        //       (612)558-4804 </a> <br>
+        //       <h5>Github:</h5>
+        //       <a href="https://github.com/Litabeach" class="links">
+        //         <img class="img img thumbnail icon" src="02-CSS-Bootstrap/Assets/Images2/githubicon.png" alt ="github icon">
+        //      https://github.com/Litabeach   </a> 
+        //       <h5>LinkedIn:</h5>
+        //       <a href="https://www.linkedin.com/in/lita-beach" class="links">
+        //         <img class="img img thumbnail icon" src="02-CSS-Bootstrap/Assets/Images2/linkedinicon.jpg" alt="linkedin icon">
+        //        https://www.linkedin.com/in/lita-beach </a>
+        //     </div>
+        
+        //   </div>
+        //   <br>
+        //   <br>
+        // <Container fluid>
+        //   <Row>
+        //     <Col size="md-6">
+    <div>
+                <h1>Contact info</h1>
+      
+ 
+        </div>
+      );
+    }
+  
+  
+  export default Contact;
