b596ce92957b1490451c1947d42b36e328d0d5f2
Lita Beach <litabeach123@gmail.com>
2021-05-03 11:40:00 -0500

    navbar and react router working in harmony

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,31 +1,81 @@
 import React from "react";
 import "./style.css";
+import { Link } from "react-router-dom";
+
+<nav classNameName="navbar navbar-expand-lg navbar-light bg-light">
+    <Link classNameName="navbar-brand" to="/">
+        GitHub JobWatcher
+</Link>
+    <div>
+        <ul classNameName="navbar-nav">
+            <li classNameName="nav-item">
+                <Link
+                    to="/"
+                    classNameName={
+                        window.location.pathname === "/" || window.location.pathname === "/home"
+                            ? "nav-link active"
+                            : "nav-link"
+                    }
+                >
+                    Home
+      </Link>
+            </li>
+        </ul>
+    </div>
+</nav>
 
 function Nav() {
-  return (
-    <nav class="navbar navbar-dark navbar-expand-md justify-content-center">
-    <a href="index.html" class="navbar-brand d-flex w-50 mr-auto">Lita Beach</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsingNavbar3">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="navbar-collapse collapse w-100" id="collapsingNavbar3">
-      <ul class="nav navbar-nav ml-auto w-100 justify-content-end">
-        <li class="nav-item">
-          <a class="nav-link" href="index.html">About <span class="sr-only">(current)</span></a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link active" href="contact.html">Contact</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="portfolio.html">Portfolio</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="02-CSS-Bootstrap/Assets/LitaBeachResume.pdf">Resume</a>
-        </li>
-      </ul>
-    </div>
-  </nav>
-  );
+    return (
+        <nav className="navbar navbar-dark navbar-expand-md justify-content-center">
+            <Link className="navbar-brand d-flex w-50 mr-auto" to="/">Lita Beach</Link>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsingNavbar3">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="navbar-collapse collapse w-100" id="collapsingNavbar3">
+                <ul className="nav navbar-nav ml-auto w-100 justify-content-end">
+                    <li className="nav-item">
+                        <Link
+                            to="/"
+                            className={
+                                window.location.pathname === "/" || window.location.pathname === "/home"
+                                    ? "nav-link active"
+                                    : "nav-link"
+                            }
+                        >
+                            About
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link
+                            to="/contact"
+                            className={
+                                window.location.pathname === "/contact" 
+                                    ? "nav-link active"
+                                    : "nav-link"
+                            }
+                        >
+                           Contact
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link
+                            to="/portfolio"
+                            className={
+                                window.location.pathname === "/portfolio" 
+                                    ? "nav-link active"
+                                    : "nav-link"
+                            }
+                        >
+                            Portfolio
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="02-CSS-Bootstrap/Assets/LitaBeachResume.pdf">Resume</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
 }
 
 export default Nav;
